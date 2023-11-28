cf1ff098e300054929444f13d006a743ff56d104
Hugo Perinazzo <hugo.perinazzo@own.security>
2023-11-28 17:36:17 +0100

    feat: debugging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// use clap::{App, Arg}
+use std::io;
+
 fn main() {
-    println!("Hello, MY new world!");
+    println!("Hello, What's you name ?");
+
+    let mut buffer: String = String::new();
+    io::stdin().read_line(&mut buffer).expect("Hello");
+
+    if buffer == "nobody\n" {
+        println!("Hello");
+    } else {
+        println!("Unauthorized user");
+    }
 }
